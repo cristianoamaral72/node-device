f63a452dd5f95fdac0867842c23642b66567a423
Leonardo Thibes <leonardothibes@gmail.com>
2018-02-20 16:32:32 -0300

    wip

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const detector = require('./lib/detector');
+const detector = require('./lib/detector'),
+      Detected = require('./lib/detected');
 
-exports.detect = detector.detect;
+/**
+ * Detects a device info.
+ *
+ * @param {String} [userAgent] If not provided, "window.navigator.userAgent" will be assumed.
+ *
+ * @return {Detected}
+ */
+exports.detect = function(userAgent)
+{
+    userAgent = userAgent || window.navigator.userAgent;
+
+    return detector.detect(userAgent);
+};
